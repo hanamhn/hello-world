6292af25de7d026b68f08d5179107eef60a4c315
hanamhn <hanamhndev@gmail.com>
2022-06-02 14:37:14 +0700

    react state & working with events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -22,8 +23,14 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,28 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [counter, setCounter] = useState(1);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
