9c7753004a29ad7fc90694085d5d40225bad35d2
hanamhn <hanamhndev@gmail.com>
2022-06-08 22:30:40 +0700

    finish

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,6 +19,7 @@ const Cart = (props) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
 
+  console.log(cartCtx);
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
